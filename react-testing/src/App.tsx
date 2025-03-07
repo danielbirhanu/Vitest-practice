11a1014a069f7ebc9b7108bbb9473a10c1eb7770
danielbirhanu <danielbir49@gmail.com>
2025-03-07 09:08:42 +0300

    Testing list components

--- react-testing/src/App.tsx
+++ react-testing/src/App.tsx
@@ -1,10 +1,14 @@
 import HelloWorld from "./components/HelloWorld"
+import UserStatus from "./components/UserStatus"
+import ProgrammingLanguageList from "./components/ProgrammingLanguageList"
 
 
 const App = () => {
   return (
     <div>
       <HelloWorld />
+      <UserStatus email="daniel@gmail.com"/>
+      <ProgrammingLanguageList languages={["amharic", "english"]} />
     </div>
   )
 }
